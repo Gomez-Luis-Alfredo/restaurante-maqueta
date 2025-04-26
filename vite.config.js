68d51fdd619ab2bd0b57bbf7ae95563d1a254988
Gomez-Luis-Alfredo <alvisael@gmail.com>
2025-04-25 21:29:39 -0300

    quinta

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  outDir: './dist', 
+  assetsDir: 'assets', 
+  copyPublicDir: false,
+  base:'./'
   
 })
